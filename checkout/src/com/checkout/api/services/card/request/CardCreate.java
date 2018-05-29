1979f322e8ef5ece2d437ccbd5c3a82cf81b0a7b
Abdul Vasim <abdulvasim786@gmail.com>
2018-05-29 19:20:55 +0530

    token field is needed ckoAPIClient.cardService.createCard(String customerId, CardCreate payload)

--- checkout/src/com/checkout/api/services/card/request/CardCreate.java
+++ checkout/src/com/checkout/api/services/card/request/CardCreate.java
@@ -3,4 +3,7 @@ package com.checkout.api.services.card.request;
 public class CardCreate extends CardUpdate{
 	public String cvv; 
 	public String number;
+	
+	// token field is needed ckoAPIClient.cardService.createCard(String customerId, CardCreate payload)
+	public String token;
 }
