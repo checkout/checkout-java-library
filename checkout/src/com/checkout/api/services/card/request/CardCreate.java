81db710883e8a683b30fcca7eba3b49817c7472f
Nicolas <nicolas-maalouf-cko@users.noreply.github.com>
2018-06-04 15:21:31 +0100

    Update CardCreate.java

--- checkout/src/com/checkout/api/services/card/request/CardCreate.java
+++ checkout/src/com/checkout/api/services/card/request/CardCreate.java
@@ -3,7 +3,5 @@ package com.checkout.api.services.card.request;
 public class CardCreate extends CardUpdate{
 	public String cvv; 
 	public String number;
-	
-	// token field is needed ckoAPIClient.cardService.createCard(String customerId, CardCreate payload)
 	public String token;
 }
